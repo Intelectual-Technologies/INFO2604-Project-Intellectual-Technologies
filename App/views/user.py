ce807f8fc4d638a81b43eb398b7b9d5f1b97a303
jjdlabee <jjdlabee@gmail.com>
2025-03-30 00:38:41 -0400

    Repurposed website and images

--- App/views/user.py
+++ App/views/user.py
@@ -38,3 +38,8 @@ def create_user_endpoint():
 @user_views.route('/static/users', methods=['GET'])
 def static_user_page():
   return send_from_directory('static', 'static-user.html')
+
+@user_views.route('/index-temp', methods=['GET'])
+def get_index_page():
+    users = get_all_users()
+    return render_template('index-temp.html')
