8d4d6cb471da35483975eb00962e486fdc0f43b3
jjdlabee <jjdlabee@gmail.com>
2025-04-04 15:33:17 -0400

    Added model controllers

---
+++ App/controllers/recipe.py
@@ -0,0 +1,33 @@
+from App.models import Ingredient, Category, Recipe
+from App.database import db
+
+def add_ingredient(id, name, description):
+    ingredient = Ingredient(id=id, name=name, description=description)
+    db.session.add(ingredient)
+    db.session.commit()
+    return ingredient
+
+def add_category(id, name, description, image):
+    category = Category(id=id, name=name, description=description, image=image)
+    db.session.add(category)
+    db.session.commit()
+    return category
+
+def add_recipe(id, name, instructions, image, yt, category_name):
+    recipe = Recipe(id=id, name=name, instructions=instructions, image=image, yt=yt, category_name=category_name)
+    db.session.add(recipe)
+    db.session.commit()
+    return recipe
+
+def add_recipe_ingredient(recipe_id, ingredient_name):
+    ingredient = Ingredient.query.filter_by(name=ingredient_name).first()
+    recipe = Recipe.query.filter_by(id=recipe_id).first()
+
+    if not ingredient:
+        return False
+
+    if recipe.ingredient == None or ingredient not in recipe.ingredient:
+        recipe.ingredient.append(ingredient)
+        db.session.add(recipe)
+        db.session.commit()
+    return True
